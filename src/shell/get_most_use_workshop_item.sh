4eeb00f72f4526f11e35992dd2aaa5b5d697f32a
zsnmwy <szlszl35622@gmail.com>
2021-07-25 21:46:45 +0800

    fix git config

--- src/shell/get_most_use_workshop_item.sh
+++ src/shell/get_most_use_workshop_item.sh
@@ -2,6 +2,6 @@
 set -Eeuo pipefail
 rm -rf ./public/top-mod-item.txt
 
-for i in {1..5}; do
+for i in {1..1}; do
   curl https://steamcommunity.com/workshop/browse/\?appid\=322330\&requiredtags%5B0%5D\=all_clients_require_mod\&actualsort\=trend\&p\="${i}"\&browsesort\=trend\&days\=-1 | grep -o 'sharedfile_.*' | cut -d '"' -f1 | cut -d '_' -f2 | uniq  >> ./public/top-mod-item.txt
 done
